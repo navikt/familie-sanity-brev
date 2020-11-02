1cf7263634c0756517bcc7973961d69195900b33
Asbjørn Steinskog <asbjorn@steinskog.me>
2020-11-02 08:13:34 +0100

    Fjern tittel-style

--- schemas/Dokumentmal.js
+++ schemas/Dokumentmal.js
@@ -2,11 +2,6 @@ import React, { useEffect, useState } from "react";
 import FlettefeltAnnontering from "./annonteringer/FlettefeltAnnontering";
 import SubmalAnnontering from "./annonteringer/SubmalAnnontering";
 import ValgfeltAnnontering from "./annonteringer/ValgfeltAnnontering";
-import styles from "../styles/myStyling.css";
-
-const TittelStyle = props => (
-  <h3 className={styles.tittel}>{props.children}</h3>
-)
 
 export default {
   title: "Dokumentmal",
@@ -14,10 +9,11 @@ export default {
   type: "document",
   preview: {
     select: {
-      title: "tittel",
+      title: "id",
     },
   },
   fields: [
+    { type: "string", title: "ID", name: "id" },
     { type: "string", title: "Tittel", name: "tittel" },
     {
       name: "innhold",
@@ -40,19 +36,14 @@ export default {
             ],
           },
           styles: [
-            { title: 'Normal', value: 'normal' },
-            {
-              title: 'Tittel', value: 'tittel', blockEditor: {
-                render: TittelStyle
-              }
-            },
-            { title: 'H1', value: 'h1' },
-            { title: 'H2', value: 'h2' },
-            { title: 'H3', value: 'h3' },
-            { title: 'H4', value: 'h4' },
-            { title: 'H5', value: 'h5' },
-            { title: 'H6', value: 'h6' },
-          ]
+            { title: "Normal", value: "normal" },
+            { title: "H1", value: "h1" },
+            { title: "H2", value: "h2" },
+            { title: "H3", value: "h3" },
+            { title: "H4", value: "h4" },
+            { title: "H5", value: "h5" },
+            { title: "H6", value: "h6" },
+          ],
         },
       ],
     },
