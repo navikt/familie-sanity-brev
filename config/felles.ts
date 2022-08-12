e72b1c8034e63c399d3d08a56f068fd041a6cbe8
halvormundal <halvorbmundal@gmail.com>
2022-08-12 13:41:37 +0200

    Legg til annen logikk for erEf. Legg til erBa. Legg til ekskluderesForBa. Legg til begrunnelse i ekskluderesForEf

--- config/felles.ts
+++ config/felles.ts
@@ -1,13 +1,20 @@
 import client from 'part:@sanity/base/client';
 
-import { DokumentNavn } from '../src/util/typer';
+import { BegrunnelseDokumentNavn, DokumentNavn } from '../src/util/typer';
 
 export const ekskluderesForEf: string[] = [
   DokumentNavn.DELMAL,
   DokumentNavn.DOKUMENT,
   DokumentNavn.PERIODE,
+  BegrunnelseDokumentNavn.BEGRUNNELSE,
+];
+
+export const ekskluderesForBa: string[] = [
+  DokumentNavn.AVANSERT_DELMAL,
+  DokumentNavn.AVANSERT_DOKUMENT,
 ];
 
 const { dataset } = client.config();
 
-export const erEf = () => dataset === 'ef-brev';
+export const erEf = () => ['ef-brev', 'ef-test', 'testdata'].includes(dataset);
+export const erBa = () => ['ba-brev', 'ba-test', 'testdata'].includes(dataset);
