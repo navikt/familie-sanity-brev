ab414ba16540fbc23c8c7f4eab61d4ff03cd502a
Kristine Steine <kristine.steine@bekk.no>
2022-11-01 15:31:47 +0100

    Fjern regelfunksjon som har blitt erstattet

--- src/schemas/ba-sak/begrunnelse/nasjonaleTriggere/utils.ts
+++ src/schemas/ba-sak/begrunnelse/nasjonaleTriggere/utils.ts
@@ -18,11 +18,3 @@ export const hentNasjonaleTriggereRegler = rule =>
     rule,
     'En nasjonal begrunnelse-trigger er valgt, men behandlingstema for begrunnelsen er ikke nasjonal.',
   );
-
-export const lagNasjonalFeltObligatoriskRegel = rule =>
-  rule.custom((currentValue, { document }) => {
-    if (erNasjonalBegrunnelse(document) && currentValue === undefined) {
-      return 'Feltet mangler verdi';
-    }
-    return true;
-  });
