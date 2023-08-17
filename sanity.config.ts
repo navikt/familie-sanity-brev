4158f58764404c861870607ada11acbab4ef9531
halvormundal <halvorbmundal@gmail.com>
2023-08-17 13:06:13 +0200

    Fiks SSO

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig, definePlugin } from 'sanity';
+import { AuthConfig, defineConfig, definePlugin } from 'sanity';
 import { deskTool } from 'sanity/desk';
 import { visionTool } from '@sanity/vision';
 import { schemaTypes } from './src';
 import { structure } from './config/deskStructure';
+
+const PROSJEKT_ID = 'xsrv1mh6';
 
 const sharedConfig = definePlugin({
   name: 'shareConfig',
@@ -12,64 +14,81 @@ const sharedConfig = definePlugin({
   },
 });
 
+const auth: AuthConfig = {
+  redirectOnSingle: true,
+  providers: () => [
+    {
+      name: 'saml',
+      title: 'NAV SSO',
+      url: 'https://api.sanity.io/v2021-10-01/auth/saml/login/f3270b37',
+      logo: '/static/navlogo.svg',
+    },
+  ],
+  loginMethod: 'dual',
+};
+
 export default defineConfig([
   {
     name: 'testdata',
     title: 'Test',
-    projectId: 'xsrv1mh6',
+    projectId: PROSJEKT_ID,
     dataset: 'testdata',
     basePath: '/testdata',
-    plugins: [deskTool()],
-    schema: {
-      types: schemaTypes,
-    },
+    plugins: [sharedConfig()],
+    auth: auth,
   },
   {
     name: 'ba-brev',
     title: 'BA',
-    projectId: 'xsrv1mh6',
+    projectId: PROSJEKT_ID,
     dataset: 'ba-brev',
     basePath: '/ba-brev',
     plugins: [sharedConfig()],
+    auth: auth,
   },
   {
     name: 'ba-test',
     title: 'BA-test',
-    projectId: 'xsrv1mh6',
+    projectId: PROSJEKT_ID,
     dataset: 'ba-test',
     basePath: '/ba-test',
     plugins: [sharedConfig()],
+    auth: auth,
   },
   {
     name: 'ks-brev',
     title: 'KS',
-    projectId: 'xsrv1mh6',
+    projectId: PROSJEKT_ID,
     dataset: 'ks-brev',
     basePath: '/ks-brev',
     plugins: [sharedConfig()],
+    auth: auth,
   },
   {
     name: 'ks-test',
     title: 'KS-test',
-    projectId: 'xsrv1mh6',
+    projectId: PROSJEKT_ID,
     dataset: 'ks-test',
     basePath: '/ks-test',
     plugins: [sharedConfig()],
+    auth: auth,
   },
   {
     name: 'ef-brev',
     title: 'EF',
-    projectId: 'xsrv1mh6',
+    projectId: PROSJEKT_ID,
     dataset: 'ef-brev',
     basePath: '/ef-brev',
     plugins: [sharedConfig()],
+    auth: auth,
   },
   {
     name: 'ef-test',
     title: 'EF-test',
-    projectId: 'xsrv1mh6',
+    projectId: PROSJEKT_ID,
     dataset: 'ef-test',
     basePath: '/ef-test',
     plugins: [sharedConfig()],
+    auth: auth,
   },
 ]);
