35e15142ad475bca0358bb3c42652baa10f779b8
halvormundal <halvorbmundal@gmail.com>
2023-08-17 10:48:43 +0200

    fritekstområde.ts: preview.component -> components.preview

--- src/schemas/avansertDokument/fritekstområde.ts
+++ src/schemas/avansertDokument/fritekstområde.ts
@@ -7,7 +7,7 @@ export const Fritekstområde = {
   fields: [
     { name: 'dummyfelt', type: 'string', initialValue: 'dummyverdi', readOnly: true, hidden: true }, // Brukes ikke til noen ting. Lagt til fordi sanity krever minst et felt
   ],
-  preview: {
-    component: FritekstområdePreview,
+  components: {
+    preview: FritekstområdePreview,
   },
 };
