7c21bc98999d1062969fffe957cb5764f6d37d3a
halvorbmundal <halvorbmundal@users.noreply.github.com>
2023-09-21 15:41:18 +0200

    Legg til felt periodeType på begrunnelser (#444)
    * Legg til felt periodeType på begrunnelser
    
    * Fiks alle steder vedtakresultat ikke var byttet ut med periodetype
    
    * periodeType -> vedtakPeriodeType

--- src/schemas/baks/begrunnelse/ba-sak/begrunnelse.tsx
+++ src/schemas/baks/begrunnelse/ba-sak/begrunnelse.tsx
@@ -26,6 +26,7 @@ import { valgbarhet } from './sanityMappeFelt/valgbarhet';
 import { begunnelseType } from './sanityMappeFelt/begrunnelsetype';
 import { begrunnelseTema } from './sanityMappeFelt/begrunnelsetema';
 import { fagsakType } from './sanityMappeFelt/fagsakType';
+import { vedtakPeriodeType } from './sanityMappeFelt/vedtakPeriodeType';
 
 const begrunnelseFlettefelt = {
   name: DokumentNavn.FLETTEFELT,
@@ -165,6 +166,7 @@ const begrunnelse = {
       name: DokumentNavn.VISNINGSNAVN,
       validation: rule => [rule.required().error('Dokumentet må ha et navn')],
     },
+    vedtakPeriodeType,
     vedtakResultat,
     begrunnelseTema,
     valgbarhet,
