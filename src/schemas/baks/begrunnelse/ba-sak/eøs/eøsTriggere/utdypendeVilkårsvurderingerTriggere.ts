4d26cc756ad168e314be7baf2e3ed407108cb538
Kristine Steine <kristine.steine@nav.no>
2023-12-01 08:44:37 +0100

    Legg til flere utdypende-valg for EØS (#505)

--- src/schemas/baks/begrunnelse/ba-sak/eøs/eøsTriggere/utdypendeVilkårsvurderingerTriggere.ts
+++ src/schemas/baks/begrunnelse/ba-sak/eøs/eøsTriggere/utdypendeVilkårsvurderingerTriggere.ts
@@ -10,6 +10,12 @@ enum UtdypendeVilkårsvurderingForEØS {
   BARN_BOR_I_STORBRITANNIA = 'BARN_BOR_I_STORBRITANNIA',
   BARN_BOR_I_STORBRITANNIA_MED_SØKER = 'BARN_BOR_I_STORBRITANNIA_MED_SØKER',
   BARN_BOR_I_STORBRITANNIA_MED_ANNEN_FORELDER = 'BARN_BOR_I_STORBRITANNIA_MED_ANNEN_FORELDER',
+  BARN_BOR_I_EØS = 'BARN_BOR_I_EØS',
+  BARN_BOR_I_EØS_MED_SØKER = 'BARN_BOR_I_EØS_MED_SØKER',
+  BARN_BOR_I_EØS_MED_ANNEN_FORELDER = 'BARN_BOR_I_EØS_MED_ANNEN_FORELDER',
+  BARN_BOR_I_NORGE = 'BARN_BOR_I_NORGE',
+  BARN_BOR_I_NORGE_MED_SØKER = 'BARN_BOR_I_NORGE_MED_SØKER',
+  BARN_BOR_ALENE_I_ANNET_EØS_LAND = 'BARN_BOR_ALENE_I_ANNET_EØS_LAND',
 }
 
 const KompetanseValg: Record<
@@ -27,6 +33,30 @@ const KompetanseValg: Record<
   BARN_BOR_I_STORBRITANNIA_MED_ANNEN_FORELDER: {
     title: 'Barn bor i Storbritannia med annen forelder',
     value: UtdypendeVilkårsvurderingForEØS.BARN_BOR_I_STORBRITANNIA_MED_ANNEN_FORELDER,
+  },
+  BARN_BOR_I_EØS: {
+    title: 'Barn bor i EØS',
+    value: UtdypendeVilkårsvurderingForEØS.BARN_BOR_I_EØS,
+  },
+  BARN_BOR_I_EØS_MED_SØKER: {
+    title: 'Barn bor i EØS med søker',
+    value: UtdypendeVilkårsvurderingForEØS.BARN_BOR_I_EØS_MED_SØKER,
+  },
+  BARN_BOR_I_EØS_MED_ANNEN_FORELDER: {
+    title: 'Barn bor i EØS med annen forelder',
+    value: UtdypendeVilkårsvurderingForEØS.BARN_BOR_I_EØS_MED_ANNEN_FORELDER,
+  },
+  BARN_BOR_I_NORGE: {
+    title: 'Barn bor i Norge',
+    value: UtdypendeVilkårsvurderingForEØS.BARN_BOR_I_NORGE,
+  },
+  BARN_BOR_I_NORGE_MED_SØKER: {
+    title: 'Barn bor i Norge med søker',
+    value: UtdypendeVilkårsvurderingForEØS.BARN_BOR_I_NORGE_MED_SØKER,
+  },
+  BARN_BOR_ALENE_I_ANNET_EØS_LAND: {
+    title: 'Barn bor alene i annet EØS-land',
+    value: UtdypendeVilkårsvurderingForEØS.BARN_BOR_ALENE_I_ANNET_EØS_LAND,
   },
 };
 
