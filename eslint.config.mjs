ec491bed1afbc8c925055dfbd6a802396a291977
gunnsteingarmo <gunnstein.hoj.garmo@nav.no>
2024-09-02 08:48:13 +0200

    Oppdatert eslint til v9 - brukt https://eslint.org/docs/latest/use/configure/migration-guide til å sette opp eslint.config.mjs

---
+++ eslint.config.mjs
@@ -0,0 +1,87 @@
+import prettier from 'eslint-plugin-prettier';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  {
+    ignores: [
+      '**/*.json',
+      '**/*.less',
+      '**/*.yaml',
+      '**/*.yml',
+      '**/*.md',
+      '**/*.html',
+      'node_modules/',
+      'plugins/',
+      'dist/',
+      '.idea/',
+    ],
+  },
+  ...compat.extends(
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    '@sanity/eslint-config-studio',
+    'prettier',
+  ),
+  {
+    plugins: {
+      prettier,
+      '@typescript-eslint': typescriptEslint,
+    },
+
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 5,
+      sourceType: 'script',
+
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: '__dirname',
+      },
+    },
+
+    rules: {
+      'no-case-declarations': 'off',
+
+      'import/extensions': [
+        'off',
+        'ignorePackages',
+        {
+          js: 'never',
+          jsx: 'never',
+          ts: 'never',
+          tsx: 'never',
+        },
+      ],
+
+      '@typescript-eslint/ban-ts-ignore': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      'prettier/prettier': 'error',
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/no-var-requires': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      'react-app/react-hooks/exhaustive-deps': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/switch-exhaustiveness-check': 'error',
+      'import/no-anonymous-default-export': 'off',
+    },
+  },
+];
