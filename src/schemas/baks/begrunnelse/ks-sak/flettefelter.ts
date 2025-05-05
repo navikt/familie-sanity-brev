3385a19dd7b159f423f19350b6dde97ed133ce97
Uy Nguyen <uy.quang.nguyen@nav.no>
2025-05-05 12:27:12 +0200

    Flettefeltet må endres til maanedOgAarFoerVedtaksperiode etter justering i ks sak

--- src/schemas/baks/begrunnelse/ks-sak/flettefelter.ts
+++ src/schemas/baks/begrunnelse/ks-sak/flettefelter.ts
@@ -3,5 +3,5 @@ import { flettefelter as baFlettefelter } from '../ba-sak/typer';
 export const flettefelter = [
   ...baFlettefelter,
   { title: 'Antall timer barnehageplass', value: 'antallTimerBarnehageplass' },
-  { title: 'Måned og år før vedtaksperiode', value: 'maanedOgAarFoorVedtaksperiode' },
+  { title: 'Måned og år før vedtaksperiode', value: 'maanedOgAarFoerVedtaksperiode' },
 ];
